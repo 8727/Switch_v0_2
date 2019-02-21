a5d39e6e8270199276f8db15ed43d361d1de34a4
Alex Li <5796630@mail.ru>
2019-02-21 12:02:50 +0300

    update

--- SOURCE/src/main.c
+++ SOURCE/src/main.c
@@ -5,17 +5,9 @@ int main(void){
   Setting();
   while(0x01){
     brg.effect = 0x01;
-//    DelayMs(1000);
-//    GuiLoadImg(0x00, 0x00, 0x00);
     DelayMs(1000);
     GuiLoadImg(0x00, 0x00, 0x01);
     DelayMs(1000);
     GuiLoadImg(0x00, 0x00, 0x02);
-    DelayMs(62);
-    GuiBr();
-    
-//    DelayMs(1000);
-//    W25QxxEraseBlocks();
-    DelayMs(2000);
   }
 }
